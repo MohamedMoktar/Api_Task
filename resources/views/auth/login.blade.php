5a45cdb17c7cd67d19ec8e5676070b6b734e05ec
mohamed mokhtar <moamedmotar755@gmail.com>
2023-04-13 02:38:28 +0200

    login ad rigster by phone numper

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,16 +12,12 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="phone_numper" class="col-md-4 col-form-label text-md-end">{{ __('Phone numper') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="phone_numper" type="text" class="form-control" name="phone_numper" value="{{ old('phone_numper') }}" autofocus>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+            
                             </div>
                         </div>
 
