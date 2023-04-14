8a4c9ae769b1dff4fe368063de7126c58167169e
mohamed mokhtar <moamedmotar755@gmail.com>
2023-04-14 15:37:55 +0200

    make seeder for data

--- database/seeders/UserOtpSeeder.php
+++ database/seeders/UserOtpSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\User_otp;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,16 @@ class UserOtpSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $ids=User::pluck('id');
+
+        for ($i=0; $i < 3 ; $i++) {
+                User_otp::create([
+                'otp'=>rand(123456,999999),
+                'user_id'=>$ids[rand(0,count($ids)-1)],
+                'expired_at'=>now(),
+               
+            ]);
+            
+        }
     }
 }
